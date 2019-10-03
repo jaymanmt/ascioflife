7c5d3c00f7c7fc0a22d4ce10992e1874c1626591
Ubuntu <ubuntu@ip-172-31-83-111.ec2.internal>
2019-10-03 15:45:09 +0000

    removed excess files, added click features to nav buttons, added home nav button

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 /*global $*/
 
+// home icon functionality
+function rotater(){
+    $("#home-icon").html(`<button style="animation-name: alerter"id="home-icon""><i class="fas fa-thermometer-three-quarters"></i></button>`);
+}
+$("#home-icon").click(function(){
+    rotater();
+})
+
+//add ripple out feature to click functionality
+$(".hvr-ripple-out").click(function(){
+    let el = $(this);
+    let clickRip = el.clone(true);
+    el.before(clickRip);
+    el.remove();
+})
+
 //DOM Ready Function
 $(function(){
     $(".pages").hide();
